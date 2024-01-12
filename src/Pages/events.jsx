728c820b69eb38f363ccf6c5b2a03a741bcab98a
John Doe <johndoe@email.com>
2024-01-12 12:59:18 -0500

    events

--- src/Pages/events.jsx
+++ src/Pages/events.jsx
@@ -11,7 +11,7 @@ const Events = () =>{
         <div >
         
             <div style={{backgroundColor:"#18151f"}} className="small-container">
-                <h1 className="font-events"> Events nice</h1>
+                <h1 className="font-events"> Events </h1>
                 <h2 className="font2-events"> 
 
 We live in an age of rapid technological development which has presented us with many benefits but also consequences. To help us better understand the ethical concerns that have arisen recently in the emerging fields of artificial intelligence, TMI is holding monthly workshops that come in various formats. They can be a talk or presentation from a guest speaker, an introduction to visualize ethical concerns with actual code and models, or opportunities to exchange and express ideas in the format of a debate.
@@ -24,7 +24,7 @@ Stay tuned to our website and other social media accounts for updates!</h2>
             <div >
             <br></br><br></br>
             <hr style={{color:"black"}} />
-            <h1 className="font3"  > Projects </h1>
+            <h1 className="font3"  > Events </h1>
             <hr style={{color:"black"}} />
 
             <br></br><br></br>
@@ -32,8 +32,8 @@ Stay tuned to our website and other social media accounts for updates!</h2>
             {events2.map((event, index) => (
 
             <div class="card " style={{width: "45rem", height:"40rem"}}>
-                <img class="card-img-top" src={event.imagePath} alt={event.name} style={{width:"717.6px",height:"403.63px",objectFit:"cover"}}/>
-                <div class="card-body card-footer">
+                <img class="card-top" src={event.imagePath} alt={event.name} style={{width:"717.6px",height:"403.63px",objectFit:"cover"}}/>
+                <div class="card-body">
                 <br></br>
 
                     <h3 class="card-title"  style={{fontFamily:"50px",color:"black"}}>{event.name}</h3>
